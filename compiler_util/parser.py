7abf1157434bc82ccef74745760714a1fa9ac20f
MoritzSchittenhelm <78039560+JartC0ding@users.noreply.github.com>
2021-12-04 11:51:27 +0100

    nvm typo

--- compiler_util/parser.py
+++ compiler_util/parser.py
@@ -501,7 +501,7 @@ class Parser:
                             node = self.__assign(self.__current_token.value)
                         elif self.__current_token.value == "cock":
                             node = self.__assign(self.__current_token.value)
-                        elif self.__current_token.valu in ("uint8", "uint16", "uint32", "uint64"):
+                        elif self.__current_token.value in ("uint8", "uint16", "uint32", "uint64"):
                             node = self.__assign(self.__current_token.value)
                         elif self.__current_token.value in ("int8", "int16", "int32", "int64"):
                             node = self.__assign(self.__current_token.value)
